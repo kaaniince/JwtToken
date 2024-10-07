7cf482533914ef748f7fbb11d6f22a21cdc161cb
Kaan Ince <kaaniince@outlook.com>
2024-10-07 19:12:33 +0300

    TokenGenerator is created

--- JwtToken/FrmTokenGenerator.cs
+++ JwtToken/FrmTokenGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JwtToken.JWT;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,7 +20,9 @@ namespace JwtToken
 
         private void btnCreateToken_Click(object sender, EventArgs e)
         {
-
+            TokenGenerator tokenGenerator = new TokenGenerator();
+            string token = tokenGenerator.GenerateJWTToken();
+            richTextBox1.Text = token;
         }
     }
 }
---
+++ JwtToken/JWT/TokenGenerator.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtToken.JWT
+{
+    public class TokenGenerator
+    {
+        public string GenerateJWTToken()
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("A much longer secret phase that meets the required length"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claim = new[]
+            {
+        new Claim(JwtRegisteredClaimNames.Sub, "test1"),
+        new Claim(JwtRegisteredClaimNames.Email, "kaaniince@outlook.com"),
+        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+    };
+
+            var token = new JwtSecurityToken(
+                issuer: "localhost",
+                audience: "localhost",
+                claims: claim,
+                expires: DateTime.Now.AddMinutes(5),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
